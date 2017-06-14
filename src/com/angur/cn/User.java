6cc53eb765ef6d8ace2856097fd5e1cc34ff60e7
Angur <pmwang@yeah.net>
2017-06-14 23:21:30 +0800

    添加了User类

---
+++ src/com/angur/cn/User.java
@@ -0,0 +1,43 @@
+/**  
+ * Project Name:TestJava  
+ * File Name:User.java  
+ * Package Name:com.angur.cn  
+ * Date:2017年6月14日下午11:11:03  
+ * Copyright (c) 2017, chenzhou1025@126.com All Rights Reserved.  
+ *  
+*/
+
+package com.angur.cn;
+
+/**
+ * ClassName: User <br/>
+ * Description: TODO ADD FUNCTION. <br/>
+ * Reason: TODO ADD REASON. <br/>
+ * Date: 2017年6月14日 下午11:11:03 <br/>
+ * 
+ * @author Angur
+ * @version
+ * @since JDK 1.8.0_131
+ * @see
+ */
+public class User {
+
+	private String name;
+	private int age;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+}
