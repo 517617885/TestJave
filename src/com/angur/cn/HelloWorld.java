d910c7b2b91ef9c6750b6dca33472fffc5e831b4
Angur <pmwang@yeah.net>
2017-06-14 22:55:27 +0800

    测试删除代码

--- src/com/angur/cn/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-package com.angur.cn;
-
-public class HelloWorld {
-
-	public static void main(String[] args) {
-		
-		System.out.println("测试案例");
-	}
-}
--- src/com/angur/cn/Person.java
+++
@@ -1,5 +0,0 @@
-package com.angur.cn;
-
-public class Person {
-
-}
